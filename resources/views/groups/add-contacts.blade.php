d55e7c873879cded5d5d6a4121986382d52c348a
ayoubelmotaoukil001 <ayoubelmotaoukil001@gmail.com>
2026-01-30 16:46:43 +0100

    feat (add-contacts.blade.php) add  assigner un contatc to n group

--- resources/views/groups/add-contacts.blade.php
+++ resources/views/groups/add-contacts.blade.php
@@ -1,9 +1,9 @@
-<h2>إضافة كونتاكت إلى مجموعة: {{ $group->name }}</h2>
+<h2>Add contact to the group {{ $group->name }}</h2>
 
 @foreach($contacts as $contact)
     <form method="POST" action="{{ route('groups.attachContact', [$group->id, $contact->id]) }}">
         @csrf
         {{ $contact->name }} ({{ $contact->email }})
-        <button type="submit">إضافة</button>
+        <button type="submit">add</button>
     </form>
 @endforeach
